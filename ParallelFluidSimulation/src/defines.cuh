4fa75ea5c3c45894375510ebd60f978c5feebfd2
thomasirmer <thomasirmer@gmail.com>
2015-01-18 14:01:06 +0100

    added Vector2D

---
+++ ParallelFluidSimulation/src/defines.cuh
@@ -0,0 +1,17 @@
+/*
+ * defines.cuh
+ *
+ *  Created on: 18.01.2015
+ *      Author: thomasirmer
+ */
+
+#ifndef DEFINES_CUH_
+#define DEFINES_CUH_
+
+struct Vector2D {
+    float x;
+    float y;
+};
+
+
+#endif /* DEFINES_CUH_ */
--- ParallelFluidSimulation/src/main.cu
+++ ParallelFluidSimulation/src/main.cu
@@ -1,6 +1,19 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "defines.cuh"
+
+#define SIM_WIDTH  512
+#define SIM_HEIGHT 512
 
 int main (void) {
 	printf("Starting CUDA-Application - Parallel Fluid Simulation ...\n");
+
+	// TODO: initialize all the stuff
+	Vector2D* vectorField = (Vector2D*) malloc(sizeof(Vector2D) * SIM_HEIGHT * SIM_WIDTH);
+
+	// TODO: do simulation (ihno)
+
+	// TODO: visualize (thomas)
 }
